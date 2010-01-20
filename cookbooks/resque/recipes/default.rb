adbb8d70e28614e3035cbc6cc946562200f5d7c9
Aaron Quint <aaron@quirkey.com>
2010-01-20 10:52:36 -0500

    Setup resque initializer

--- cookbooks/resque/recipes/default.rb
+++ cookbooks/resque/recipes/default.rb
@@ -18,7 +18,12 @@ end
 gem_package "resque" do 
   source "http://gemcutter.org"
   action :install
-end  
+end
+
+gem_package "resque-status" do
+  source "http://gemcutter.org"
+  action :install
+end
 
 gem_package "uuid" do 
   source "http://gemcutter.org"
@@ -47,6 +52,17 @@ node[:applications].each do |app, data|
       :num_workers => 2,
       :app_name => app,
       :rails_env => node[:environment][:framework_env]
+    })
+  end                  
+  
+  template "/data/#{app}/shared/config/resque.rb" do
+    owner node[:owner_name]
+    group node[:owner_name]
+    mode 0755
+    source "initializer.rb.erb"
+    variables({
+      :redis_path => "#{node[:master_app_server][:private_dns_name]}:6379",
+      :expire_in => (14 * 24 * 60 * 60) # 2 weeks
     })
   end
   
